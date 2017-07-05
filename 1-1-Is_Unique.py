cdb052bedbbb4740c476ceeaaf3d692c4f06b1e2
michaelhua0125 <410763264@qq.com>
2017-07-05 17:28:12 -0400

    optimization

--- 1-1-Is_Unique.py
+++ 1-1-Is_Unique.py
@@ -3,12 +3,14 @@ def unique(input_str):
     if len(input_str)>128: ## more than total number of ASCII characters
         return False
 
-    char_dict={}
+    char_set=[False for _ in range(128)]
+
     for i in range(len(input_str)):
-        if char_dict.get(input_str[i],None)!=None:
+        key=ord(input_str[i])
+        if char_set[key]==True:
             return False
         else:
-            char_dict[input_str[i]]=1
+            char_set[key]=True
 
     return True
 
