96162fa713bf3efc01e9bab92e131fa61c74ff35
michaelhua0125 <410763264@qq.com>
2017-07-09 08:48:25 -0400

    adding class LinkedList

--- linked_list.py
+++ linked_list.py
@@ -6,5 +6,34 @@ class Node:
     def __str__(self):
         return str(self.data)
 
-    def append(self,node_y):
-        self.next=node_y
+    # def append(self,node_y):
+    #     self.next=node_y
+
+
+class LinkedList:
+    def __init__(self,data=None):
+        ## set head
+        self.head=Node(data)
+        self.head.next=None
+
+        ## set current pointer position
+        self.current=head
+
+        ## set counter
+        self.counter=1
+
+    def __str__(self):
+        temp_list=[] ## list of nodes
+        pointer=head
+        while pointer.next!=None:
+            temp_list.append(pointer.data)
+            pointer=pointer.next
+        temp_list.append(pointer)
+
+        return ','.join(temp_list)
+
+
+    def append(self,data):
+        self.current.next=Node(data)
+        self.current=self.current.next
+        self.counter+=1
